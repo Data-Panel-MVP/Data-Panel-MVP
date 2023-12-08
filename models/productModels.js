ff5fb4e66b61e403c2bb7776d1ecb6d73b118fc8
ayushhh321 <ayush56039@gmail.com>
2023-12-08 21:13:35 +0530

    crud

---
+++ models/productModels.js
@@ -0,0 +1,189 @@
+const mongoose = require('mongoose')
+
+// Subdocument schema for 'desc'
+const descSchema = new mongoose.Schema({
+    specifications: {
+        type: Map,
+        of: mongoose.Schema.Types.Mixed,
+        required: true,
+    },
+}, { _id: false });//using id=false to disbale automatic id generation in mongoDB
+
+// Subdocument schema for 'attrs'
+const attrsSchema = new mongoose.Schema({
+    design: {
+        type: String,
+        required: true, //values where user needs to click to see changes in variation
+    },
+    imgs: {
+        type: String,
+        required: true,
+    },
+}, { _id: false });//using id=false to disbale automatic id generation in mongoDB
+
+// Subdocument schema for 'design'
+const designSchema = new mongoose.Schema({//it consist of all the feilds that inside a variation
+    product_id: {
+        type: String,
+        required: [true,],//validation method
+    },
+
+    labourPerFloor: {
+        type: Number,
+        required: [true,],//validation method
+    },
+
+    applicability: {
+        type: Number,
+        required: [true,],//validation method
+    },
+
+    loadingUnloadingPrice: {
+        type: Number,
+        required: [true,],//validation method
+    },
+    price: {
+        type: Number,
+        required: [true,],//validation method
+    },
+
+    discounted_price: {
+        type: Number,
+        required: [true,],//validation method
+    },
+
+    discounted_percent: {
+        type: Number,
+        required: [true,],//validation method
+    },
+
+    desc: descSchema, //calling function for desc bcz desc is dynamic
+    attrs: attrsSchema, //calling function for design and image inside of attrs bcz attrs is dynamic
+}, { _id: false });
+
+// Main schema for 'vars'
+const varsSchema = new mongoose.Schema({
+    design: [designSchema], // Array of designs
+}, { _id: false });
+
+
+const productSchema = mongoose.Schema(//basic product model consist of all parameter including vars
+    {
+        group: {
+            type: String,
+            required: [true,],//validation method
+        },
+
+
+        subgroup: {
+            type: String,
+            required: [true,],//validation method  
+        },
+
+        brand: {
+            type: String,
+            required: [true,],//validation method
+        },
+
+        product_id: {
+            type: String,
+            required: [true,],//validation method
+        },
+
+        plyUnit: {
+            type: Number,
+            required: [true,],//validation method
+        },
+
+        labourPerFloor: {
+            type: Number,
+            default: 0,
+        },
+
+        applicability: {
+            type: Number,
+            required: [true,],//validation method
+        },
+
+        loadingUnloadingPrice: {
+            type: Number,
+            default: 0,
+        },
+
+        product_name: {
+            type: String,
+            required: [true,],//validation method
+        },
+
+        ProductDescription: {
+            type: String,
+            required: [true,],//validation method
+        },
+
+
+        category: {
+            type: String,
+            required: [true,],//validation method
+        },
+
+        subcategory: {
+            type: String,
+            required: [true,],//validation method
+        },
+
+        attrs: {
+            imgs: {
+                type: [String], // an array of strings image
+                required: [true, "Please provide at least one image URL"],
+            }
+        },
+
+        averageRating: {
+            type: Number,
+            required: [true,],
+            default: 0
+        },
+
+        price: {
+            type: Number,
+            required: [true,],//validation method
+        },
+
+        discounted_price: {
+            type: Number,
+            required: [true,],//validation method
+        },
+
+        discounted_percent: {
+            type: Number,
+            required: [true,],//validation method
+        },
+
+        desc: descSchema,//desc/specification of product model is also dynamic thats why calling the same function that we called in vars.
+
+        vars: varsSchema,//declared vars before productModel and vars is nested of attrs and desc
+
+
+        //this two are made for future purpose
+        // NumberOfItemInStock: {
+        //     type: Number,
+        //     required: [true,],//validation method
+        //     default: 0
+        // },
+
+        //   StockINorOUT: {
+        //     type: Boolean,
+        //     required: [true,],//validation method
+        // }
+
+
+    },
+    {
+        timestamps: true//it will give time and date everytime you add data 
+    }
+)
+
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
---
+++ server.js
@@ -0,0 +1,96 @@
+//added server.js in package.json so use npm run serve
+
+
+
+const express = require('express')
+const mongoose = require('mongoose')//conected to mongodb
+const Product = require('./models/productModels')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+//routes- we have to declare routes so that web browser access the our web page
+
+app.get('/', (req, res) => {
+    res.send('Hello Node API (changing to see nodemon')
+})
+
+app.get('/blog', (req, res) => {
+    res.send('Hello testing GET')
+})
+
+app.get('/products', async(req, res) => {//for fetching or getting data from database
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/products/:id', async(req, res) =>{//fetching product via ID
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+app.post('/products', async(req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
+//update a product
+app.put('/products/:id', async(req, res) => {//here route is PUT make sure to use PUT in postman to getting products
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        // we cannot find any product in database
+        if(!product){
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+// // delete a product
+
+app.delete('/products/:id', async(req, res) =>{//here route is DELETE make sure to use DELETE in postman to getting products
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        }
+        res.status(200).json(product);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+mongoose.set("strictQuery", false)
+mongoose.connect('mongodb+srv://AYUSH:9835456039@cluster0.hofxigh.mongodb.net/?retryWrites=true&w=majority')//connected to atlas
+.then(() => {
+    console.log('connected to MongoDB')//when app is conncted to mongodb it will display this message 
+    app.listen(3000, ()=> {
+        console.log(`App is running on port 3000 <ayush bhai>`)//after connected to mongodb it will shows App is running..
+    });
+}).catch((error) => {
+    console.log(error)//if there is any issue it will shows "error"
+})
